6bfdafddfc9cce8f6625e4e58a89eccbf2865604
KunalGurav-FLUX <k.gurav@finregacademy.com>
2020-07-02 12:39:21 +0530

    Minor Change

--- src/Redux/Actions/Actions.js
+++ src/Redux/Actions/Actions.js
@@ -1,24 +1,24 @@
 export const counterincrement = () => {
-    return {
-        type: 'INCREASE_COUNTER'
-    }
-}
+  return {
+    type: 'INCREASE_COUNTER',
+  };
+};
 export const counterdecrement = () => {
-    return {
-        type: 'DECREASE_COUNTER'
-    }
-}
+  return {
+    type: 'DECREASE_COUNTER',
+  };
+};
 
-export const counterset = (v) => {
-    return {
-        type: 'COUNTER_SET',
-        value: v
-    }
-}
-export const signup = (email,pass) => {
-    return {
-        type: 'SignUp',
-        Email:email,
-        Password:pass
-    }
-}
+export const counterset = v => {
+  return {
+    type: 'COUNTER_SET',
+    value: v,
+  };
+};
+export const signup = (email, pass) => {
+  return {
+    type: 'SignUp',
+    Email: email,
+    Password: pass,
+  };
+};
